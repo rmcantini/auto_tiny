b740177c6a4528bab168a54318a38eb148550f10
rmcantini <rmcantini@gmail.com>
2022-05-02 22:41:56 -0300

    Successfully wrote the code for sorting files by size

---
+++ filter_files_test.py
@@ -0,0 +1,38 @@
+''' Script to select only files with a specific size in a folder '''
+import os
+import tkinter
+from tkinter.filedialog import askdirectory
+
+# intro explanation pop-up
+tkinter.messagebox.showinfo(
+    'info', 'Selecione a pasta com os desdobramentos a serem organizados.')
+
+# asks what directory to work with
+path_main = askdirectory(
+    initialdir='~/downloads',
+    title='Selecione a pasta onde estão os arquivos')
+names = os.listdir(path_main)
+
+
+def weight(each_file):
+    ''' Get list of all files only in the given directory '''
+    return os.path.isfile(os.path.join(path_main, each_file))
+
+
+files_list = filter(weight, os.listdir(path_main))
+
+# Create a list of files in directory along with the size
+size_of_file = [
+    (f, os.stat(os.path.join(path_main, f)).st_size)
+    for f in files_list]
+
+# Iterate over list of files along with size
+for f, s in size_of_file:
+    kb_size = s
+    MAX_SIZE = 250000
+
+    # Check if the file is too heavy
+    if kb_size >= MAX_SIZE:
+        print(f'Found, {f}: {kb_size}')
+    else:
+        print(f'no compress, {f}:{kb_size}')
